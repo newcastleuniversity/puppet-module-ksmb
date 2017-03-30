51ff4a1e1a58d1f818c7f696edfb94c35a640fa5
Helen Griffiths <helen.griffiths@newcastle.ac.uk>
2017-03-30 16:48:03 +0100

    rename pcounter module to ksmb

---
+++ manifests/config.pp
@@ -0,0 +1,68 @@
+class ksmb::config inherits ksmb {
+
+  file { '/etc/systemd/system/cups.service' :
+    ensure  => file,
+    replace => true,
+    owner   => root,
+    group   => root,
+    source  => "puppet:///modules/${module_name}/cups.service",
+    mode    => '0444',
+    notify  => Service[$ksmb::service],
+  }
+
+  $ksmb::ppdfiles.each |$ppdfile| {
+
+    file { "${ksmb::ppdpath}/${ppdfile}" :
+      ensure  => file,
+      replace => true,
+      owner   => 'root',
+      group   => 'root',
+      source  => "${ksmb::ppdsource}/${ppdfile}",
+      mode    => '0444',
+      notify  => Service[$ksmb::service],
+    }
+
+  }
+
+  $ksmb::filterfiles.each |$filterfile| {
+
+    file { "${ksmb::filterpath}/${filterfile}" :
+      ensure  => file,
+      replace => true,
+      owner   => 'root',
+      group   => 'root',
+      source  => "${ksmb::ppdsource}/${filterfile}",
+      mode    => '0555',
+      notify  => Service[$ksmb::service],
+    }
+
+  }
+
+  file { '/etc/sudoers.d/lp' :
+    ensure  => file,
+    replace => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    content => "lp ALL=(ALL,!root) NOPASSWD: /usr/lib/cups/backend/ksmb.real\n",
+  }
+
+  file { '/usr/lib/cups/backend/ksmb' :
+    ensure  => file,
+    replace => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0555',
+    content => "#!/bin/dash\nsudo -u \$2 /usr/lib/cups/backend/ksmb.real \$DEVICE_URI \"\$@\"\n",
+  }
+
+  file { '/usr/lib/cups/backend/ksmb.real' :
+    ensure  => file,
+    replace => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0555',
+    source  => "puppet:///modules/${module_name}/ksmb.real",
+  }
+
+}
---
+++ manifests/install.pp
@@ -0,0 +1,9 @@
+class ksmb::install inherits ksmb {
+
+  $ksmb::package_list.each |$package| {
+
+    package { $package: }
+
+  }
+
+}
