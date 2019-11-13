b748973e668d5a495e49bb50d59f164e90142360
Helen Griffiths <helen.griffiths@newcastle.ac.uk>
2019-11-13 18:03:41 +0000

    fix metadata.json; add default params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,61 +1,18 @@
-# Class: ksmb
-# ===========================
-#
-# Full description of class ksmb here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'ksmb':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2017 Your name here, unless otherwise noted.
-#
+# install a ksmb backend for kerberised printing
 
 class ksmb (
 
-  Optional[String] $ppdpath,
-  Optional[String] $ppdsource,
-  Optional[Array[String]] $ppdfiles,
-  Optional[Array[String]] $package_list,
-  Optional[String] $service
+  Optional[String] $ppdpath             = '/usr/share/cups/model',
+  Optional[String] $ppdsource           = 'puppet:///modules/ksmb/ppds',
+  Optional[Array[String]] $ppdfiles     = ['KOC658UX.ppd'],
+  Optional[Array[String]] $package_list = ['smbclient'],
+  Optional[String] $service             = 'cups'
 
 ){
 
-  anchor { '::ksmb::begin': } ->
-  class { '::ksmb::install': } ~>
-  class { '::ksmb::config': } ->
-  anchor { '::ksmb::end': }
+  anchor { '::ksmb::begin': }
+  -> class { '::ksmb::install': }
+  ~> class { '::ksmb::config': }
+  -> anchor { '::ksmb::end': }
 
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe 'ksmb' do
   context 'with default values for all parameters' do
-    it { should contain_class('ksmb') }
+    it { is_expected.to contain_class('ksmb') }
   end
 end
