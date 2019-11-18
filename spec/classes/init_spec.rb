6919364496611ab8c9de7df9bd0b352d29440cef
Helen Griffiths <helen.griffiths@newcastle.ac.uk>
2019-11-18 14:45:18 +0000

    add basic smoke tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 describe 'ksmb' do
   context 'with default values for all parameters' do
-    it { is_expected.to contain_class('ksmb') }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_class('ksmb::config') }
+    it { is_expected.to contain_class('ksmb::install') }
   end
 end
